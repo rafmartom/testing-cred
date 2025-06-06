ae4d2d9c8f933330a5243fcef188e66270ee9379
Freddie Ventura <creativefreddieventura@gmail.com>
2025-06-06 15:05:03 +0100

    Adding saying something

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ function sayHello () {
     console.log('I am saying hello');
 }
 
+function saySomething () {
+    console.log('I am saying something');
+}
+
 function main () {
     sayHello();
+    saySomething();
 }
 
 main();
